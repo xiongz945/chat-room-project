6b7e7b3df545c644dcbd04a17cfea0354bc0e7f9
ZSShen <andy.zsshen@gmail.com>
2019-09-17 16:20:36 -0700

    Modify: Apply syntax suger to handle promise returned from Axios.

--- frontend/src/pages/login/login.js
+++ frontend/src/pages/login/login.js
@@ -1,74 +1,59 @@
 import {SHA256} from "../../utils/hash.js";
+import userApis from "../../apis/user-apis.js";
 
 
-async function post(data) {
-
-  const response = await axios({
-    method: "post",
-    url: "http://localhost:3000/users/login",
-    data: data
-  });
-
-  return response;
-}
-
-
-function validateForm(event) {
+async function validateForm(event) {
   event.preventDefault();
 
   const username = document.getElementById("username");
   const password = document.getElementById("password");
 
   const passwordHash = SHA256(password.value);
-  const data = {
+  let data = {
     "username": username.value,
     "password": passwordHash
   };
 
-  const response = post(data);
-  response.then(function(payload) {
-    const message = payload["data"]["message"];
+  let response = await userApis.login(data);
+  let message = response["data"]["message"];
 
-    if (message == "authenticated") {
-      // Log into the system and then do nothing.
-      window.location.replace("http://localhost:4000/");
-    } else if (message == "invalid user") {
-      const intended = confirm("Do you want to create a new account?");
+  if (message == "authenticated") {
+    // Log into the system and then do nothing.
+    window.location.replace("http://localhost:4000/");
+    return;
+  }
 
-      if (intended) {
-        // Notify the backend to create a new account.
-        const data = {
-          "username": username.value,
-          "password": passwordHash,
-          "confirm": true
-        };
+  if (message == "invalid password") {
+    // Pop an alert message and reset the input text boxes.
+    alert("Oops! Invalid password...");
+    username.value = "";
+    password.value = "";
+    return;
+  }
 
-        const response = post(data);
-        response.then(function(payload) {
-          const message = payload["data"]["message"];
+  // The case that the username is not existing.
+  const intended = confirm("Do you want to create a new account?");
+  if (!intended) {
+    // Reset the input text boxes.
+    username.value = "";
+    password.value = "";
+    return;
+  }
 
-          if (message == "registered") {
-            // Show the welcome message.
+  // Notify the backend to create a new account.
+  data["confirm"] = true;
+  response = await userApis.login(data);
+  message = response["data"]["message"];
 
-          } else {
-            // Reset the input text boxes.
-            alert("Oops! Unexpected result...");
-            username.value = "";
-            password.value = "";
-          }
-        });
-      } else {
-        // Reset the input text boxes.
-        username.value = "";
-        password.value = "";
-      }
-    } else if (message == "invalid password") {
-        // Reset the input text boxes.
-        alert("Oops! Invalid password...");
-        username.value = "";
-        password.value = "";
-    }
-  });
+  if (message == "registered") {
+    // Show the welcome message.
+
+  } else {
+    // Reset the input text boxes.
+    alert("Oops! Unexpected result...");
+    username.value = "";
+    password.value = "";
+  }
 }
 
 
