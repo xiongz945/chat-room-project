689c787c3ca220d5e02ee390c3f765255acb20ce
ZSShen <andy.zsshen@gmail.com>
2019-10-11 22:18:49 -0700

    Add: Draft auth controller test.

--- backend/src/controllers/auth.ts
+++ backend/src/controllers/auth.ts
@@ -25,6 +25,74 @@ export const passwordRule = check(
   min: 4,
 });
 
+const serveExistingUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  passport.authenticate(
+    'local',
+    { session: false },
+    (err: Error, user: IUserDocument, info: IVerifyOptions) => {
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        return res.status(400).json({ message: ['invalid password'] });
+      }
+      req.logIn(user, { session: false }, (err) => {
+        if (err) {
+          return next(err);
+        }
+        const plainUserObject = {
+          username: user.username,
+          password: user.password,
+        };
+        const token = jwt.sign(plainUserObject, JWT_SECRET);
+        return res.status(200).json({
+          user: plainUserObject,
+          token,
+          message: ['authenticated'],
+        });
+      });
+    }
+  )(req, res, next);
+};
+
+const serveNonExistingUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (req.body.confirm == true) {
+    try {
+      const newUser = await User.createNewUser({
+        username: req.body.username,
+        password: req.body.password,
+      });
+      req.logIn(newUser, { session: false }, (err) => {
+        if (err) {
+          return next(err);
+        }
+        const plainUserObject = {
+          username: newUser.username,
+          password: newUser.password,
+        };
+        const token = jwt.sign(plainUserObject, JWT_SECRET);
+        return res.status(200).json({
+          user: plainUserObject,
+          token,
+          message: ['registered'],
+        });
+      });
+    } catch (err) {
+      return next(err);
+    }
+  } else {
+    return res.status(400).json({ message: ['non-existing username'] });
+  }
+};
+
 /**
  * POST /login
  * Login or create a new account.
@@ -35,6 +103,7 @@ export const postLogin = async (
   next: NextFunction
 ) => {
   const errors = validationResult(req);
+
   if (!errors.isEmpty()) {
     const errMsgs: any[] = [];
     errors.array().forEach((element) => {
@@ -45,61 +114,9 @@ export const postLogin = async (
   try {
     const existingUser = await User.findUserByName(req.body.username);
     if (existingUser) {
-      passport.authenticate(
-        'local',
-        { session: false },
-        (err: Error, user: IUserDocument, info: IVerifyOptions) => {
-          if (err) {
-            return next(err);
-          }
-          if (!user) {
-            return res.status(400).json({ message: ['invalid password'] });
-          }
-          req.logIn(user, { session: false }, (err) => {
-            if (err) {
-              return next(err);
-            }
-            const plainUserObject = {
-              username: user.username,
-              password: user.password,
-            };
-            const token = jwt.sign(plainUserObject, JWT_SECRET);
-            return res.status(200).json({
-              user: plainUserObject,
-              token,
-              message: ['authenticated'],
-            });
-          });
-        }
-      )(req, res, next);
+      return await serveExistingUser(req, res, next);
     } else {
-      if (req.body.confirm == true) {
-        try {
-          const newUser = await User.createNewUser({
-            username: req.body.username,
-            password: req.body.password,
-          });
-          req.logIn(newUser, { session: false }, (err) => {
-            if (err) {
-              return next(err);
-            }
-            const plainUserObject = {
-              username: newUser.username,
-              password: newUser.password,
-            };
-            const token = jwt.sign(plainUserObject, JWT_SECRET);
-            return res.status(200).json({
-              user: plainUserObject,
-              token,
-              message: ['registered'],
-            });
-          });
-        } catch (err) {
-          return next(err);
-        }
-      } else {
-        return res.status(400).json({ message: ['non-existing username'] });
-      }
+      return await serveNonExistingUser(req, res, next);
     }
   } catch (err) {
     return next(err);
--- backend/test/models/user.test.ts
+++ backend/test/models/user.test.ts
@@ -1,31 +1,27 @@
 import mongoose from 'mongoose';
 
-import {User} from '../../src/models/User';
-import {MONGODB_URI, SESSION_SECRET} from '../../src/config/secrets';
-
+import { User } from '../../src/models/User';
+import { MONGODB_URI, SESSION_SECRET } from '../../src/config/secrets';
 
 describe('Test User Model', () => {
-
-  beforeAll( async() => {
+  beforeAll(async () => {
     mongoose
-    .connect(MONGODB_URI, { useNewUrlParser: true })
-    .then(() => {
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+      .connect(MONGODB_URI, { useNewUrlParser: true })
+      .then(() => {})
+      .catch((err) => {
+        console.log(err);
+      });
 
-    const user = new User({username: '123', password: '123'});
+    const user = new User({ username: '123', password: '123' });
     await user.save();
-  })
+  });
 
-  afterAll( async() => {
-    await User.deleteOne({username: '123'});
-  })
+  afterAll(async () => {
+    await User.deleteOne({ username: '123' });
+  });
 
   test('findUserByName() can find an exsiting user', async () => {
     const existingUser = await User.findUserByName('123');
     expect(existingUser['username']).toBe('123');
   });
-})
-
+});
