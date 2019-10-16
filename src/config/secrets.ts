0a976d77def5cb42dec73dd8d3f057a850c5b18b
Xiangl5 <xiangl5@andrew.cmu.edu>
2019-10-16 07:07:28 +0000

    Change env vars config

--- src/config/secrets.ts
+++ src/config/secrets.ts
@@ -2,20 +2,14 @@ import logger from '../util/logger';
 import dotenv from 'dotenv';
 import fs from 'fs';
 
-if (fs.existsSync('.env')) {
-  logger.debug('Using .env file to supply config environment variables');
-  dotenv.config({ path: '.env' });
-} else {
-  logger.debug(
-    'Using .env.example file to supply config environment variables'
-  );
-  dotenv.config({ path: '.env.example' }); // you can delete this after you create your own .env file!
-}
+// Load env variables
+dotenv.config({ path: '.env' });
+
 export const ENVIRONMENT = process.env.NODE_ENV;
 const prod = ENVIRONMENT === 'production'; // Anything else is treated as 'dev'
 
-export const SESSION_SECRET = process.env['SESSION_SECRET'];
-export const JWT_SECRET: string = 'eqweqweqweq';
+export const SESSION_SECRET: string = process.env['SESSION_SECRET'];
+export const JWT_SECRET: string = process.env['JWT_SECRET'];
 
 export const MONGODB_URI = prod
   ? process.env['MONGODB_URI']
