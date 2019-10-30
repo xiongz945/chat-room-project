be724775649f526537bbc9919058ef014d4591d2
ZSShen <andy.zsshen@gmail.com>
2019-10-29 23:07:43 -0700

    Add: Finish auth api integration test.

--- test/api/auth.test.ts
+++ test/api/auth.test.ts
@@ -4,13 +4,10 @@ import server from '../../src/server';
 import { MONGODB_URI, setMongoDbUri } from '../../src/config/secrets';
 import { User } from '../../src/models/User';
 
-
 setMongoDbUri(MONGODB_URI + '_auth');
 const mock = supertest(server);
 
-
 describe('Auth API', () => {
-
   beforeAll(async () => {
     const user = new User({ username: '123', password: '1234' });
     await user.save();
@@ -21,12 +18,24 @@ describe('Auth API', () => {
   });
 
   test('POST /auth/login ', async () => {
-
     const res = await mock.post('/auth/login').send({
-      'username': '123',
-      'password': '1234'
+      username: '123',
+      password: '1234',
     });
 
+    expect(res.status).toEqual(200);
+  });
+
+  test('GET /auth/logout', async () => {
+    const login = await mock.post('/auth/login').send({
+      username: '123',
+      password: '1234',
+    });
+
+    const token = login.body.token;
+    const res = await mock
+      .get('/auth/logout')
+      .set('Authorization', `Bearer ${token}`);
     expect(res.status).toEqual(200);
   });
 });
