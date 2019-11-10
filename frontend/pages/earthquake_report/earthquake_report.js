15701eb5530c793f7e27def1df25c45e42e3605c
Can <canx@andrew.cmu.edu>
2019-11-10 03:16:29 -0800

    Add: Mapbox component

--- frontend/pages/earthquake_report/earthquake_report.js
+++ frontend/pages/earthquake_report/earthquake_report.js
@@ -6,3 +6,57 @@ if (userStore.userGetters.isLogin) {
     userStore.userGetters.user().username
   }!`;
 }
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiY2FueCIsImEiOiJjazJzbTZ2eGMwbXMyM2JsN3VwZzlpOTIyIn0.M0NE8kywhhrC1pDQ9j_kww';
+let map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+});
+
+let marker = new mapboxgl.Marker({
+  draggable: true,
+});
+
+map.on('load', function() {
+  navigator.geolocation.getCurrentPosition((position) => {
+    const userCoordinates = [
+      position.coords.longitude,
+      position.coords.latitude,
+    ];
+    map.addSource('user-coordinates', {
+      type: 'geojson',
+      data: {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: userCoordinates,
+        },
+      },
+    });
+    map.addLayer({
+      id: 'user-coordinates',
+      source: 'user-coordinates',
+      type: 'circle',
+    });
+
+    map.flyTo({
+      center: userCoordinates,
+      zoom: 14,
+    });
+    marker.setLngLat(userCoordinates).addTo(map);
+    let coordinates = document.getElementById('coordinates');
+    coordinates.innerHTML =
+      'Longitude: ' +
+      userCoordinates[0] +
+      '<br />Latitude: ' +
+      userCoordinates[1];
+    function onDragEnd() {
+      let lngLat = marker.getLngLat();
+      // coordinates.style.display = 'block';
+      coordinates.innerHTML =
+        'Longitude: ' + lngLat.lng + '<br />Latitude: ' + lngLat.lat;
+    }
+    marker.on('dragend', onDragEnd);
+  });
+});
