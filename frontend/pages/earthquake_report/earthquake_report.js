8b3828b5146c63e95547a7dec9c6b2fa9a73a7f8
Can <canx@andrew.cmu.edu>
2019-11-09 23:14:17 -0800

    Add: Earthquake report page.

---
+++ frontend/pages/earthquake_report/earthquake_report.js
@@ -0,0 +1,8 @@
+import userStore from '../../store/user.js';
+
+if (userStore.userGetters.isLogin) {
+  document.getElementById('join-community-button').style.display = 'none';
+  document.getElementById('welcome-message').innerText = `Welcome, ${
+    userStore.userGetters.user().username
+  }!`;
+}
