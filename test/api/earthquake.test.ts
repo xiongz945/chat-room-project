7ee82253772751f2223613b743e3e8630e5a02af
Can <canx@andrew.cmu.edu>
2019-11-15 19:41:49 -0800

    Add: Refined API tests.

--- test/api/earthquake.test.ts
+++ test/api/earthquake.test.ts
@@ -16,15 +16,39 @@ describe('Earthquake API', () => {
   let citizenToken: string;
   let coordinatorToken: string;
   let patchReportID: string;
+  const postedReport = {
+    occurred_datetime: new Date(),
+    description: 'test_post_report',
+    magnitude: 2.0,
+    location: {
+      longitude: 0.0,
+      latitude: 0.0,
+    },
+    killed: 0,
+    injured: 0,
+    missing: 0,
+  };
+  let patchedPayload: any;
+  const postedPrediction = {
+    occurred_datetime: new Date(),
+    description: 'test_post_prediction',
+    magnitude: 2.0,
+    location: {
+      longitude: 0.0,
+      latitude: 0.0,
+    },
+  };
   beforeAll(async () => {
     const citizen = new User({ username: 'citizen_user', password: '123456' });
     await citizen.save();
+
     const coordinator = new User({
       username: 'coordinator_user',
       password: '123456',
       role: 'coordinator',
     });
     await coordinator.save();
+
     const testGetReport = new EarthquakeReport({
       occurred_datetime: new Date(),
       description: 'test_get_report',
@@ -39,6 +63,7 @@ describe('Earthquake API', () => {
       reporterName: 'a',
     });
     await testGetReport.save();
+
     const testPatchReport = new EarthquakeReport({
       occurred_datetime: new Date(),
       description: 'test_patch_report',
@@ -57,6 +82,22 @@ describe('Earthquake API', () => {
       description: 'test_patch_report',
     }).exec();
     patchReportID = res._id;
+    patchedPayload = {
+      report_id: patchReportID,
+      report: {
+        occurred_datetime: new Date(),
+        description: 'test_patch_report',
+        magnitude: 2.0,
+        location: {
+          longitude: 0.0,
+          latitude: 0.0,
+        },
+        killed: 1,
+        injured: 1,
+        missing: 1,
+        reporterName: 'citizen_user',
+      },
+    };
   }, 100000);
 
   afterAll(async () => {
@@ -78,33 +119,26 @@ describe('Earthquake API', () => {
     coordinatorToken = res.body.token;
   });
 
-  test('POST /earthquake/report', async () => {
-    const report = {
-      occurred_datetime: new Date(),
-      description: 'test1',
-      magnitude: 2.0,
-      location: {
-        longitude: 0.0,
-        latitude: 0.0,
-      },
-      killed: 0,
-      injured: 0,
-      missing: 0,
-      reporterName: 'a',
-    };
-    let res = await mock.post('/earthquake/report').send(report);
+  test('POST /earthquake/report - Unauthorized', async () => {
+    const res = await mock.post('/earthquake/report').send(postedReport);
     expect(res.status).toEqual(401);
-    res = await mock
+  });
+
+  test('Post /earthquake/report - Authorized', async () => {
+    const res = await mock
       .post('/earthquake/report')
-      .send(report)
+      .send(postedReport)
       .set('Authorization', `Bearer ${citizenToken}`);
     expect(res.status).toEqual(200);
   });
 
-  test('GET /earthquake/report', async () => {
-    let res = await mock.get('/earthquake/report');
+  test('GET /earthquake/report - Unauthorized', async () => {
+    const res = await mock.get('/earthquake/report');
     expect(res.status).toEqual(401);
-    res = await mock
+  });
+
+  test('GET /earthquake/report - Authorized', async () => {
+    const res = await mock
       .get('/earthquake/report')
       .set('Authorization', `Bearer ${citizenToken}`);
     let flag = false;
@@ -114,33 +148,23 @@ describe('Earthquake API', () => {
     expect(flag).toBeTruthy();
   });
 
-  test('PATCH /earthquake/report', async () => {
-    const payload = {
-      report_id: patchReportID,
-      report: {
-        occurred_datetime: new Date(),
-        description: 'test_patch_report',
-        magnitude: 2.0,
-        location: {
-          longitude: 0.0,
-          latitude: 0.0,
-        },
-        killed: 1,
-        injured: 1,
-        missing: 1,
-        reporterName: 'citizen_user',
-      },
-    };
-    let res = await mock.patch('/earthquake/report').send(payload);
+  test('PATCH /earthquake/report - without token', async () => {
+    const res = await mock.patch('/earthquake/report').send(patchedPayload);
     expect(res.status).toEqual(401);
-    res = await mock
+  });
+
+  test('PATCH /earthquake/report - with unmatched token', async () => {
+    const res = await mock
       .patch('/earthquake/report')
-      .send(payload)
+      .send(patchedPayload)
       .set('Authorization', `Bearer ${coordinatorToken}`);
     expect(res.status).toEqual(401);
-    res = await mock
+  });
+
+  test('PATCH /earthquake/report - with matched token', async () => {
+    const res = await mock
       .patch('/earthquake/report')
-      .send(payload)
+      .send(patchedPayload)
       .set('Authorization', `Bearer ${citizenToken}`);
     expect(res.status).toEqual(200);
     const queryRes = await EarthquakeReport.findById(patchReportID).exec();
@@ -149,26 +173,25 @@ describe('Earthquake API', () => {
     expect(queryRes.missing).toEqual(1);
   });
 
-  test('POST /earthquake/prediction', async () => {
-    const prediction = {
-      occurred_datetime: new Date(),
-      description: 'test_patch_report',
-      magnitude: 2.0,
-      location: {
-        longitude: 0.0,
-        latitude: 0.0,
-      },
-    };
-    let res = await mock.post('/earthquake/prediction').send(prediction);
-    expect(res.status).toEqual(401);
-    res = await mock
+  test('POST /earthquake/prediction - without token', async () => {
+    const res = await mock
       .post('/earthquake/prediction')
-      .send(prediction)
+      .send(postedPrediction);
+    expect(res.status).toEqual(401);
+  });
+
+  test('POST /earthquake/prediction - with citizen token', async () => {
+    const res = await mock
+      .post('/earthquake/prediction')
+      .send(postedPrediction)
       .set('Authorization', `Bearer ${citizenToken}`);
     expect(res.status).toEqual(401);
-    res = await mock
+  });
+
+  test('POST /earthquake/prediction - with coordinator token', async () => {
+    const res = await mock
       .post('/earthquake/prediction')
-      .send(prediction)
+      .send(postedPrediction)
       .set('Authorization', `Bearer ${coordinatorToken}`);
     expect(res.status).toEqual(200);
   });
