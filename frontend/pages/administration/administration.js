3847580884329315b6861df174b5dc000a46f395
Can <canx@andrew.cmu.edu>
2019-11-30 15:44:22 -0800

    Add: update user profile

--- frontend/pages/administration/administration.js
+++ frontend/pages/administration/administration.js
@@ -1,4 +1,5 @@
 import userStore from '../../store/user.js';
+import { SHA256 } from '../../utils/hash.js';
 import administrationApis from '../../apis/administration-apis.js';
 
 if (userStore.userGetters.isLogin()) {
@@ -6,16 +7,23 @@ if (userStore.userGetters.isLogin()) {
   document.getElementById('welcome-message').innerText = `Welcome, ${
     userStore.userGetters.user().username
   }!`;
-  document.getElementById('logout-button').style.display = 'block';
 }
 
 getUserProfile();
 
+document
+  .querySelector('#update-profile-form')
+  .addEventListener('submit', submitUpdateProfileFormListener, true);
+
 async function getUserProfile() {
   const resp = await administrationApis.getUserProfile();
   const users = resp['data']['users'];
-  console.log(users);
   updateUserProfileTable(users);
+  document
+    .querySelectorAll('.update-profile-btn')
+    .forEach((updateProfileBtn) => {
+      updateProfileBtn.addEventListener('click', updateBtnCLickListener);
+    });
   return users;
 }
 
@@ -23,10 +31,11 @@ function updateUserProfileTable(users) {
   const profileTable = document.querySelector('#profile-table');
   users.forEach((user) => {
     const row = document.createElement('tr');
+    row.id = user['_id'];
     const usernameCol = document.createElement('td');
     usernameCol.innerText = user['username'];
     const activeStatusCol = document.createElement('td');
-    activeStatusCol.innerText = 'active' in user ? user['active'] : true;
+    activeStatusCol.innerText = user['active'] ? 'active' : 'inactive';
     const privilegeLevelCol = document.createElement('td');
     privilegeLevelCol.innerText = user['role'] || 'citizen';
     const updateCol = document.createElement('td');
@@ -40,4 +49,40 @@ function updateUserProfileTable(users) {
     row.appendChild(updateCol);
     profileTable.appendChild(row);
   });
+}
+
+function updateBtnCLickListener(event) {
+  const profileRow = event.srcElement.parentElement.parentElement;
+  document.querySelector('#update-profile-form')['user-id'] = profileRow.id;
+  document.querySelector('#update-username-input').value =
+    profileRow.children[0].innerText;
+  document.querySelector('#update-password-input').value = '';
+  document.querySelector('#update-account-status-input').value =
+    profileRow.children[1].innerText;
+  document.querySelector('#update-privilege-level-input').value =
+    profileRow.children[2].innerText;
+  $('#profile-modal').modal('show');
+}
+
+async function submitUpdateProfileFormListener() {
+  const userId = document.querySelector('#update-profile-form')['user-id'];
+  const updatedProfile = {
+    username: document.querySelector('#update-username-input').value,
+    active:
+      (document.querySelector('#update-account-status-input').value ||
+        'active') === 'active',
+    role:
+      document.querySelector('#update-privilege-level-input').value ||
+      'citizen',
+  };
+  const plainPassword = document.querySelector('#update-password-input').value;
+  if (plainPassword) {
+    updatedProfile['password'] = SHA256(plainPassword);
+  }
+  console.log(updatedProfile);
+  await administrationApis.updateUserProfile({
+    user_id: userId,
+    user: updatedProfile,
+  });
+  location.reload();
 }
