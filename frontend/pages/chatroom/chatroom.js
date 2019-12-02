cc1e41a87479dd5f4a311539513a8708c890a3ee
XiangL5 <xiangl5@andrew.cmu.edu>
2019-12-02 01:36:44 -0800

    Unify Menus Across site

--- frontend/pages/chatroom/chatroom.js
+++ frontend/pages/chatroom/chatroom.js
@@ -1,15 +1,6 @@
 import { API_ROOT } from '../../config.js';
 
-import router from '../../router.js';
-
-import messageStore from '../../store/message.js';
-import clockStore from '../../store/clock.js';
 import userStore from '../../store/user.js';
-
-import messageApis from '../../apis/message-apis.js';
-import userApis from '../../apis/user-apis.js';
-import chatroomApis from '../../apis/chatroom-apis.js';
-import searchApis from '../../apis/search-apis.js';
 
 import { setUserIsOnline, getAllUserInfo } from './utils/api-senders.js';
 import {
